a953a9cb0159eb203a28d8a71fe71dd68a9e8768
kwangure <kaf.wangure@gmail.com>
2022-12-10 14:24:37 +0300

    fix: use builtin resolver for everything

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,6 @@
 import { css } from './parse/css.js';
 import { dataToEsm } from '@rollup/pluginutils';
 import fs from 'fs';
-import path from 'path';
 import { svelte } from './parse/svelte.js';
 
 const DOCS_QUERY_RE = /.*(?=:docs)\b/;
@@ -14,31 +13,15 @@ export function plugin() {
 	return {
 		name: 'vite-plugin-docs',
 		enforce: 'pre',
-		resolveId(id, importer) {
-			if (docsImport(id)) {
-				if (importer?.endsWith('.html')) {
-					/**
-                     * We probably got here through Vite dev mode?
-                     * id: '/path/component.svelte:docs'
-                     * importer: '/home/user/project/index.html'
-                     */
-					const dir = path.dirname(importer);
-					return path.join(dir, id);
-				} else if (id.startsWith('.')) {
-					if (!importer) return id;
-					/**
-                     * We probably got here via JS import
-                     * id: './component.svelte:docs'
-                     * importer: '/home/user/project/path/docs.js'
-                     */
-					const dir = path.dirname(importer);
-					return path.resolve(dir, id);
-				}
+		async resolveId(id, importer, options) {
+			const match = docsImport(id);
+			if (match) {
+				// without ':docs' postfix
+				const [filepath] = match;
+				const resolved = await this
+					.resolve(filepath, importer, options);
 
-				/**
-                 * Somebody else resolved it? (e.g resolve.alias)
-                 */
-				return id;
+				if (resolved) return `${resolved.id}:docs`;
 			}
 		},
 		load(id) {
